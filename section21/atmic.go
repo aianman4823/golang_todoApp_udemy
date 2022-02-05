3964381579e8c2968f385b8f53c3340913a246e1
aianman4823 <akitoaianman1@gmail.com>
2022-02-05 16:15:49 +0900

    atomicを使ったmutexらしい処理

---
+++ section21/atmic.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	// var count int
+	// var mu sync.Mutex
+
+	// increment := func() {
+	// 	mu.Lock()
+	// 	count++
+	// 	mu.Unlock()
+	// }
+
+	// increment()
+
+	// fmt.Println(count)
+
+	var count int64
+
+	increment := func() {
+		atomic.AddInt64(&count, 1)
+	}
+
+	increment()
+
+	fmt.Println(count)
+}
